3fd881c2e2b01b4645d5e1c38b6ea4ce5de8e9af
Daniel S <daniel.sadovnik95@gmail.com>
2025-08-11 16:14:17 -0300

    correçoes e adiçao da versao pdf

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function br(){
-    document.getElementById("contents").innerHTML = "Português";
+const buttonBR = document.getElementById("buttonBR")
+const buttonUS = document.getElementById("buttonUS")
+
+const contents = document.getElementById("contents")
+
+function brTranslation() {
+   alert("Em construção...")
 }
+
+function usTranslation() {
+   alert("Under construction...")
+}
+
+
+buttonBR.addEventListener("click", brTranslation);
+buttonUS.addEventListener("click", usTranslation);
